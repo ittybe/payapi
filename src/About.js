8e887198d45c42eec7654cd6ad8cc7f48cd491bf
michael <ittybemain@gmail.com>
2021-10-15 19:11:59 +0200

    commit

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+export class About extends react.Component {
+    render() {
+        return (
+            <div>About</div>
+        )
+    }
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+export class Home extends react.Component {
+    render() {
+        return (
+            <div>Home</div>
+        )
+    }
+}
+
+export default Home;
