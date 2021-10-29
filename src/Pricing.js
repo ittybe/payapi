8d225e6f7d2d48a05ca6d712f54a6ad02fd85498
michael <ittybemain@gmail.com>
2021-10-29 20:47:08 +0200

    pricing html tree

---
+++ src/Pricing.js
@@ -0,0 +1,71 @@
+import react from "react";
+import "./Pricing.css";
+
+export class Pricing extends react.Component {
+    render() {
+        return (
+            <div className="pricing-page">
+                <div className="pricing-section">
+                    <div className="plan">
+                        <div className="plan__heading">Free Plan</div>
+                        <div className="plan__about">Build and test using our core set of products with up to 100 API requests</div>
+                        <div className="plan__price">
+                            $0.00
+                        </div>
+                        <div className="plan__pros">
+                            <div className="plan__pro plan__pro--unactive">Transactions</div>
+                            <div className="plan__pro plan__pro--unactive">Auth</div>
+                            <div className="plan__pro plan__pro--unactive">Identity</div>
+                            <div className="plan__pro plan__pro--unactive">Investments</div>
+                            <div className="plan__pro plan__pro--unactive">Assets</div>
+                            <div className="plan__pro plan__pro--unactive">Liabilities</div>
+                            <div className="plan__pro plan__pro--unactive">Income</div>
+                        </div>
+                        <div className="btn btn--hollow btn--blue">Request Access</div>
+                    </div>
+                    <div className="plan">
+                        <div className="plan__heading">Basic Plan</div>
+                        <div className="plan__about">Launch your project with unlimited requests and no contractual minimums
+                        </div>
+                        <div className="plan__price">
+                            $249.00
+                        </div>
+                        <div className="plan__pros">
+                            <div className="plan__pro plan__pro--unactive">Transactions</div>
+                            <div className="plan__pro plan__pro--unactive">Auth</div>
+                            <div className="plan__pro plan__pro--unactive">Identity</div>
+                            <div className="plan__pro plan__pro--unactive">Investments</div>
+                            <div className="plan__pro plan__pro--unactive">Assets</div>
+                            <div className="plan__pro plan__pro--unactive">Liabilities</div>
+                            <div className="plan__pro plan__pro--unactive">Income</div>
+                        </div>
+                        <div className="btn btn--hollow btn--blue">Request Access</div>
+                    </div>
+                    <div className="plan">
+                        <div className="plan__heading">Premium Plan</div>
+                        <div className="plan__about"> Get tailored solutions, volume pricing, and dedicated support for your team
+                        </div>
+                        <div className="plan__price">
+                            $499.00
+                        </div>
+                        <div className="plan__pros">
+                            <div className="plan__pro plan__pro--unactive">Transactions</div>
+                            <div className="plan__pro plan__pro--unactive">Auth</div>
+                            <div className="plan__pro plan__pro--unactive">Identity</div>
+                            <div className="plan__pro plan__pro--unactive">Investments</div>
+                            <div className="plan__pro plan__pro--unactive">Assets</div>
+                            <div className="plan__pro plan__pro--unactive">Liabilities</div>
+                            <div className="plan__pro plan__pro--unactive">Income</div>
+                        </div>
+                        <div className="btn btn--hollow btn--blue">Request Access</div>
+                    </div>
+                </div>
+                <div className="email-section ">
+                    <div className="email-section__heading">Ready to start?</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default About;
