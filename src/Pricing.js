e30f1e288f92df747396f610eecb9b59943d3a82
michael <ittybemain@gmail.com>
2021-10-30 21:57:51 +0200

    pricing styles mobile

--- src/Pricing.js
+++ src/Pricing.js
@@ -4,8 +4,9 @@ import "./Pricing.css";
 export class Pricing extends react.Component {
     render() {
         return (
-            <div className="pricing-page">
-                <div className="pricing-section page-margin-x">
+            <div className="pricing-page page-margin-x">
+                <div className="pricing-page__heading">Pricing</div>
+                <div className="pricing-section">
                     <div className="plan">
                         <div className="plan__heading">Free Plan</div>
                         <div className="plan__about">Build and test using our core set of products with up to 100 API requests</div>
@@ -13,15 +14,15 @@ export class Pricing extends react.Component {
                             $0.00
                         </div>
                         <div className="plan__pros">
-                            <div className="plan__pro plan__pro--unactive">Transactions</div>
-                            <div className="plan__pro plan__pro--unactive">Auth</div>
-                            <div className="plan__pro plan__pro--unactive">Identity</div>
+                            <div className="plan__pro">Transactions</div>
+                            <div className="plan__pro">Auth</div>
+                            <div className="plan__pro">Identity</div>
                             <div className="plan__pro plan__pro--unactive">Investments</div>
                             <div className="plan__pro plan__pro--unactive">Assets</div>
                             <div className="plan__pro plan__pro--unactive">Liabilities</div>
                             <div className="plan__pro plan__pro--unactive">Income</div>
                         </div>
-                        <div className="btn btn--hollow btn--blue">Request Access</div>
+                        <div className="btn btn--hollow-blue">Request Access</div>
                     </div>
                     <div className="plan">
                         <div className="plan__heading">Basic Plan</div>
@@ -31,15 +32,15 @@ export class Pricing extends react.Component {
                             $249.00
                         </div>
                         <div className="plan__pros">
-                            <div className="plan__pro plan__pro--unactive">Transactions</div>
-                            <div className="plan__pro plan__pro--unactive">Auth</div>
-                            <div className="plan__pro plan__pro--unactive">Identity</div>
-                            <div className="plan__pro plan__pro--unactive">Investments</div>
-                            <div className="plan__pro plan__pro--unactive">Assets</div>
+                            <div className="plan__pro">Transactions</div>
+                            <div className="plan__pro">Auth</div>
+                            <div className="plan__pro">Identity</div>
+                            <div className="plan__pro">Investments</div>
+                            <div className="plan__pro">Assets</div>
                             <div className="plan__pro plan__pro--unactive">Liabilities</div>
                             <div className="plan__pro plan__pro--unactive">Income</div>
                         </div>
-                        <div className="btn btn--hollow btn--blue">Request Access</div>
+                        <div className="btn btn--hollow-blue">Request Access</div>
                     </div>
                     <div className="plan">
                         <div className="plan__heading">Premium Plan</div>
@@ -49,15 +50,15 @@ export class Pricing extends react.Component {
                             $499.00
                         </div>
                         <div className="plan__pros">
-                            <div className="plan__pro plan__pro--unactive">Transactions</div>
-                            <div className="plan__pro plan__pro--unactive">Auth</div>
-                            <div className="plan__pro plan__pro--unactive">Identity</div>
-                            <div className="plan__pro plan__pro--unactive">Investments</div>
-                            <div className="plan__pro plan__pro--unactive">Assets</div>
-                            <div className="plan__pro plan__pro--unactive">Liabilities</div>
-                            <div className="plan__pro plan__pro--unactive">Income</div>
+                            <div className="plan__pro">Transactions</div>
+                            <div className="plan__pro">Auth</div>
+                            <div className="plan__pro">Identity</div>
+                            <div className="plan__pro">Investments</div>
+                            <div className="plan__pro">Assets</div>
+                            <div className="plan__pro">Liabilities</div>
+                            <div className="plan__pro">Income</div>
                         </div>
-                        <div className="btn btn--hollow btn--blue">Request Access</div>
+                        <div className="btn btn--hollow-blue">Request Access</div>
                     </div>
                 </div>
                 <div className="email-section page-margin-x">
