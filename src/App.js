1d5828c38a58567d2461bc6a4bd80094165a8849
michael <ittybemain@gmail.com>
2021-11-13 10:23:42 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -48,111 +48,111 @@ export class App extends react.Component {
     }
     render() {
         return (
-                <Router>
-                    <div className={`page-wrapper ${this.state.isMenuHidden ? "" : "scroll-lock"}`}>
-                        <header className="header flex flex-row page-margin-x">
-                            <div className="header__logo-link-wrapper flex items-center justify-center">
+            <Router>
+                <div className={`page-wrapper ${this.state.isMenuHidden ? "" : "scroll-lock"}`}>
+                    <header className="header flex flex-row page-margin-x">
+                        <div className="header__logo-link-wrapper flex items-center justify-center">
+                            <Link to="/payapi">
+                                <img src={logo} alt="logo" />
+                            </Link>
+                        </div>
+                        <nav className={`header__nav ${(this.state.isMenuHidden ? "hidden" : "")}`}>
+                            <ul className="header__link-list flex flex-row items-center">
+                                <li className="header__link">
+                                    <Link to="/payapi/pricing">Pricing</Link>
+                                </li>
+                                <li className="header__link">
+                                    <Link to="/payapi/about">About</Link>
+                                </li>
+                                <li className="header__link">
+                                    <Link to="/payapi/contact">Contact</Link>
+                                </li>
+                            </ul>
+                            <button className="header__schedule-button btn btn--pink-filled">
+                                Schedule a Demo
+                            </button>
+                        </nav>
+                        <nav className={`header__nav-mobile ${(this.state.isMenuHidden ? "hidden" : "")}`}>
+                            <ul className="header__link-list-mobile flex flex-row items-center">
+                                <li className="header__link-mobile">
+                                    <Link to="/payapi/pricing">Pricing</Link>
+                                </li>
+                                <li className="header__link-mobile">
+                                    <Link to="/payapi/about">About</Link>
+                                </li>
+                                <li className="header__link-mobile">
+                                    <Link to="/payapi/contact">Contact</Link>
+                                </li>
+                            </ul>
+                            <button className="header__schedule-button-mobile btn btn--pink-filled">
+                                Schedule a Demo
+                            </button>
+                        </nav>
+                        <button className={`header__show-hide-menu ${this.state.isMenuHidden ? "" : "header__show-hide-menu--hide-menu"}`} onClick={() => this.showHideMenu()}>
+                        </button>
+                    </header>
+                    <div>
+                        <Switch>
+                            <Route path="/contact">
+                                <Contact />
+                            </Route>
+                            <Route path="/pricing">
+                                <Pricing />
+                            </Route>
+                            <Route path="/about">
+                                <About />
+                            </Route>
+                            <Route path="/">
+                                <Home />
+                            </Route>
+                        </Switch>
+                    </div>
+                    <div className="email-section text-center page-margin-x">
+                        <div className="email-section__heading">Ready to start?</div>
+                        {/* email component */}
+                        <div className="email-wrapper email-wrapper--bottom">
+                            <InputEmail />
+                        </div>
+                    </div>
+                    <footer className="text-center page-padding-x">
+                        <div className="footer__content-wrapper">
+                            {/* logo */}
+                            <div className="logo inline-block">
                                 <Link to="/payapi">
                                     <img src={logo} alt="logo" />
                                 </Link>
                             </div>
-                            <nav className={`header__nav ${(this.state.isMenuHidden ? "hidden" : "")}`}>
-                                <ul className="header__link-list flex flex-row items-center">
-                                    <li className="header__link">
+                            {/* links */}
+                            <nav className="nav-bottom">
+                                <ul>
+                                    <li>
                                         <Link to="/payapi/pricing">Pricing</Link>
                                     </li>
-                                    <li className="header__link">
+                                    <li>
                                         <Link to="/payapi/about">About</Link>
                                     </li>
-                                    <li className="header__link">
+                                    <li>
                                         <Link to="/payapi/contact">Contact</Link>
                                     </li>
                                 </ul>
-                                <button className="header__schedule-button btn btn--pink-filled">
-                                    Schedule a Demo
-                                </button>
                             </nav>
-                            <nav className={`header__nav-mobile ${(this.state.isMenuHidden ? "hidden" : "")}`}>
-                                <ul className="header__link-list-mobile flex flex-row items-center">
-                                    <li className="header__link-mobile">
-                                        <Link to="/payapi/pricing">Pricing</Link>
-                                    </li>
-                                    <li className="header__link-mobile">
-                                        <Link to="/payapi/about">About</Link>
-                                    </li>
-                                    <li className="header__link-mobile">
-                                        <Link to="/payapi/contact">Contact</Link>
-                                    </li>
-                                </ul>
-                                <button className="header__schedule-button-mobile btn btn--pink-filled">
-                                    Schedule a Demo
-                                </button>
-                            </nav>
-                            <button className={`header__show-hide-menu ${this.state.isMenuHidden ? "" : "header__show-hide-menu--hide-menu"}`} onClick={() => this.showHideMenu()}>
-                            </button>
-                        </header>
-                        <div>
-                            <Switch>
-                                <Route exact path="/contact">
-                                    <Contact />
-                                </Route>
-                                <Route exact path="/pricing">
-                                    <Pricing />
-                                </Route>
-                                <Route exact path="/about">
-                                    <About />
-                                </Route>
-                                <Route exact path="/">
-                                    <Home />
-                                </Route>
-                            </Switch>
-                        </div>
-                        <div className="email-section text-center page-margin-x">
-                            <div className="email-section__heading">Ready to start?</div>
-                            {/* email component */}
-                            <div className="email-wrapper email-wrapper--bottom">
-                                <InputEmail />
+                            {/* social media */}
+                            <div className="social-media flex flex-row justify-center">
+                                <a href="#">
+                                    <img src={facebook} alt="" />
+                                </a>
+                                <a href="#">
+                                    <img src={linkedin} alt="" />
+                                </a>
+                                <a href="#">
+                                    <img src={twitter} alt="" />
+                                </a>
                             </div>
                         </div>
-                        <footer className="text-center page-padding-x">
-                            <div className="footer__content-wrapper">
-                                {/* logo */}
-                                <div className="logo inline-block">
-                                    <Link to="/payapi">
-                                        <img src={logo} alt="logo" />
-                                    </Link>
-                                </div>
-                                {/* links */}
-                                <nav className="nav-bottom">
-                                    <ul>
-                                        <li>
-                                            <Link to="/payapi/pricing">Pricing</Link>
-                                        </li>
-                                        <li>
-                                            <Link to="/payapi/about">About</Link>
-                                        </li>
-                                        <li>
-                                            <Link to="/payapi/contact">Contact</Link>
-                                        </li>
-                                    </ul>
-                                </nav>
-                                {/* social media */}
-                                <div className="social-media flex flex-row justify-center">
-                                    <a href="#">
-                                        <img src={facebook} alt="" />
-                                    </a>
-                                    <a href="#">
-                                        <img src={linkedin} alt="" />
-                                    </a>
-                                    <a href="#">
-                                        <img src={twitter} alt="" />
-                                    </a>
-                                </div>
-                            </div>
 
-                        </footer>
-                    </div>
-                </Router>
+                    </footer>
+                </div>
+            </Router>
         )
     }
 }
